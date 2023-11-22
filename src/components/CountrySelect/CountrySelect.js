795823a138781dbdca6c4e02bfc8c5dbf83ae393
Soji Jang <Hailey@Haileys-MacBook-Air.local>
2023-11-22 20:11:47 +0000

    Add background image and icons for country selecting page

--- src/components/CountrySelect/CountrySelect.js
+++ src/components/CountrySelect/CountrySelect.js
@@ -1,5 +1,26 @@
 import "./CountrySelect.scss";
+import React, { ReactDOM } from "react";
+import Globe from "../../assets/icons/earth_2035985.png";
+import PushPin from "../../assets/icons/location_6675274.png";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 
 export default function CountrySelect() {
-  return <></>;
+  const navigate = useNavigate();
+
+  const handleClickMarker = () => {
+    navigate("/dashboard");
+  };
+
+  return (
+    <div className="country-select">
+      <h2 className="country-select__text">Click</h2>
+      <img className="country-select__globe-image" src={Globe} alt="Globe" />
+      <img
+        className="country-select__pin-image"
+        src={PushPin}
+        alt="Push Pin"
+        onClick={handleClickMarker}
+      />
+    </div>
+  );
 }
--- src/pages/CountrySelection/CountrySelection.js
+++ src/pages/CountrySelection/CountrySelection.js
@@ -1,5 +1,11 @@
 import "./CountrySelection.scss";
+import CountrySelect from "../../components/CountrySelect/CountrySelect";
 
 export default function CountrySelection() {
-  return;
+  return (
+    <main className="first-page">
+      <h1 className="first-page__title">Where are you going?</h1>
+      <CountrySelect />
+    </main>
+  );
 }
