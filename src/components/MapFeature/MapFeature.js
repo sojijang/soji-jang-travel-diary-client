105c683c02cbab0e30e74007180df517f720f8f0
Soji Jang <Hailey@Haileys-MacBook-Air.local>
2023-11-23 17:09:53 +0000

    Add search box in order to allow users to search location

--- src/components/MapFeature/MapFeature.js
+++ src/components/MapFeature/MapFeature.js
@@ -1,9 +1,12 @@
 import "./MapFeature.scss";
-import Map, { Marker, Popup } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useRef, useEffect, useState } from "react";
-import MapPoint from "../MapPoint/MapPoint";
+import Map, { Marker, Popup } from "react-map-gl";
+import { SearchBox } from "@mapbox/search-js-react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+// import "@mapbox/search-js-react/css/style.css";
 import Flag from "../../assets/icons/flag_10552468.png";
+
 import {
   fetchMapPoint,
   fetchOneMapPoint,
@@ -18,7 +21,7 @@ export default function MapFeature() {
     longitude: -0.118092,
     zoom: 8,
   });
-  const [showPopup, setShowPopup] = useState(true);
+  const [showPopup, setShowPopup] = useState(false);
   const [points, setPoints] = useState([]);
 
   useEffect(() => {
@@ -34,8 +37,34 @@ export default function MapFeature() {
     getPoints();
   }, []);
 
+  const handelShowPopup = () => {
+    setShowPopup(true);
+  };
+
+  const handleOnResult = (result) => {
+    setViewState({
+      latitude: result.features[0].geometry.coordinates[1],
+      longitude: result.features[0].geometry.coordinates[0],
+      zoom: 13,
+    });
+  };
+
   return (
     <div>
+      <SearchBox
+        accessToken={process.env.REACT_APP_MAP_TOKEN}
+        onRetrieve={handleOnResult}
+        marker={true}
+        theme={{
+          variables: {
+            fontFamily: "Avenir, sans-serif",
+            unit: "14px",
+            padding: "0.5em",
+            borderRadius: "0",
+            boxShadow: "0 0 0 1px silver",
+          },
+        }}
+      />
       <Map
         id="map-container"
         mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
@@ -46,28 +75,36 @@ export default function MapFeature() {
       >
         {points.map((point) => (
           <div key={point.id}>
-            <Marker longitude={point.lng} latitude={point.lat} anchor="bottom">
+            <Marker
+              className="marker"
+              longitude={point.lng}
+              latitude={point.lat}
+              anchor="bottom"
+            >
               <img
                 className="marker__image"
                 src={Flag}
-                style={{ fontSize: viewState.zoom }}
+                style={{ font: viewState.zoom }}
                 alt="Marker"
+                onClick={handelShowPopup}
               />
             </Marker>
-            <Popup
-              longitude={point.lng}
-              latitude={point.lat}
-              anchor="top"
-              closeButton={true}
-              closeOnClick={false}
-            >
-              <div className="map-place">
-                <label htmlFor="place">Place</label>
-                <p>{point.title}</p>
-                <label htmlFor="description">Description</label>
-                <p>{point.description}</p>
-              </div>
-            </Popup>
+            {showPopup && (
+              <Popup
+                longitude={point.lng}
+                latitude={point.lat}
+                anchor="bottom"
+                closeButton={true}
+                closeOnClick={false}
+              >
+                <div className="map-place">
+                  <label htmlFor="place">Place</label>
+                  <p>{point.title}</p>
+                  <label htmlFor="description">Description</label>
+                  <p>{point.description}</p>
+                </div>
+              </Popup>
+            )}
           </div>
         ))}
       </Map>
